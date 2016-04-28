2a94c380fcd71296e8ef81f2a6fe8b4ba04f2b30
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2016-04-28 18:08:18 +0200

    add examples

---
+++ examples/Silverlight50Application/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Silverlight50Application
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            var con = new ConcurrentDictionary<string, int>();
+            con.TryAdd("cat", 1);
+            con.TryAdd("dog", 2);
+
+            // Try to update if value is 4 (this fails).
+            con.TryUpdate("cat", 200, 4);
+
+            // Try to update if value is 1 (this works).
+            con.TryUpdate("cat", 100, 1);
+        }
+    }
+}
